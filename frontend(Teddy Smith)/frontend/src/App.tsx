a252eec08249343134848afcb2f8d4dcd941c2b0
Shahin Izadi <meushahin8@gmail.com>
2025-12-28 23:12:24 +0330

    React Typescript 2023 - 5. State

--- frontend(Teddy Smith)/frontend/src/App.tsx
+++ frontend(Teddy Smith)/frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import CardList from "./Componenets/CardList/CardList";
+import Search from "./Componenets/Search/Search";
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
